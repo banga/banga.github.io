cb6bd38f46ddc928730b628e8e24e44069b23c32
Shrey Banga <banga.shrey@gmail.com>
2023-12-20 01:16:44 -0500

    Google verification for banga.github.io

--- src/page.tsx
+++ src/page.tsx
@@ -9,6 +9,7 @@ export function renderPage(title: string, children: ReactElement): string {
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="pRrkd4gy53JhwF_29of9W-qdyjE0pVe9X2Kmrtd7vZ0" />
         <title>${title}</title>
         <link rel="stylesheet" type="text/css" href="/style.css?${cacheBuster}" />
     </head>
